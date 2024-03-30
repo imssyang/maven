a42552eed795c1c9b8aeb142e26aa9c19927d5e5
imssyang <imssyang@gmail.com>
2024-03-30 23:45:17 +0800

    update env and remove meta

--- setup/env.sh
+++ setup/env.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export MAVEN_DISABLE_ENV=yes
 export M2_HOME=/opt/maven
 export MAVEN_HOME=/opt/maven
 #export MAVEN_OPTS="-Xmx1048m -Xms256m -XX:MaxPermSize=312M"
-export PATH=$MAVEN_HOME/bin:$PATH
 
